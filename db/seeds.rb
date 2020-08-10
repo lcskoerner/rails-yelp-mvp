1a6108e02158fb0baa71a276704ac8f35b6e2ad9
Lucas Koerner <lcskoerner@gmail.com>
2020-08-10 01:54:37 -0400

    seed restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Company.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w(chinese italian japanese french belgian).sample
+  )
+  restaurant.save!
+end
+
+puts "Restaurants created"
