101f7cc718f619cea185b75ca94cf136517890ca
Lucas Koerner <lcskoerner@gmail.com>
2020-08-09 22:50:20 -0400

    model validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
+  has_many :reviews, dependent: :destroy
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, inclusion: { in: (0..5) }, numericality: {
+    only_integer: true
+  }
 end
