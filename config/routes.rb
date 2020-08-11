ae8adefa405a2e310abaf69252d2c71faf5d9cd6
Lucas Koerner <lcskoerner@gmail.com>
2020-08-10 20:33:49 -0400

    controllers implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :restaurants, except: [:update, :destroy] do
-    resources :reviews, only: [ :new, :create ]
+    resources :reviews, only: [:new, :create]
   end
-
 end
